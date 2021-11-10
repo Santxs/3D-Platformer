2e12bdbfb13193bdea86b0818aaaecd6b70d0483
Gihan Akpinar <gakpinar2021@elam.co.uk>
2021-11-10 12:40:25 +0000

    I Updated the Physics

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    public float maxSpeed = 2.0f;
+    float rotation = 0.0f;
+    float camRotation = 0.0f;
+    GameObject cam;
+    Rigidbody myRigidbody;
+    float rotationSpeed = 2.0f;
+    float camRotationSpeed = 1.5f;
+
+    bool isOnGround;
+    public GameObject groundChecker;
+    public LayerMask groundLayer;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GameObject.Find("Main Camera");
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+
+        isOnGround = Physics.CheckSphere(groundChecker.transform.position, 0.1f, groundLayer);
+
+        Vector3 newVelocity = transform.forward * Input.GetAxis("Vertical") * maxSpeed;
+        myRigidbody.velocity = new Vector3(newVelocity.x, myRigidbody.velocity.y, newVelocity.z);
+
+        rotation = rotation + Input.GetAxis("Mouse X") * rotationSpeed; 
+        transform.rotation = Quaternion.Euler(new Vector3(0.0f, rotation, 0.0f));
+
+        camRotation = camRotation - Input.GetAxis("Mouse Y") * camRotationSpeed;
+        cam.transform.localRotation = Quaternion.Euler(new Vector3(camRotation, 0.0f, 0.0f));
+
+    }
+}
