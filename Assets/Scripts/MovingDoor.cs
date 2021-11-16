ff21d7797ab9e541a33814e17ca2f5bf0183da79
Santxs <gakpinar2021@elam.co.uk>
2021-11-16 20:49:48 +0000

    Moving Door Coded

---
+++ Assets/Scripts/MovingDoor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingDoor : MonoBehaviour
+{
+    public bool isOpen = false; 
+	
+	public Transform door;
+	public Transform startingPoint;
+	public Transform endingPoint; 
+	
+	// Start is called before the first frame update
+    private void Start()
+    {
+        door.position = startingPoint.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isOpen == true)
+		{
+		    door.position = Vector3.MoveTowards(door.position, endingPoint.position, Time.deltaTime);
+        }
+	}
+}
