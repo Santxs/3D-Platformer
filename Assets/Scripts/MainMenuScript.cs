403f77c4873523a122714dde8de0da9acf5f94a0
Gihan Akpinar <gakpinar2021@elam.co.uk>
2021-11-23 15:03:26 +0000

    I made a working MainMenu + OptionsMenu

---
+++ Assets/Scripts/MainMenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public void PlayTheGame()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitTheGame()
+    {
+        Application.Quit();
+    }
+}
